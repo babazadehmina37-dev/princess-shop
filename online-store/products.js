ef1ec17724e153d6e7dbf8110cf96f497e12fb4e
babazadehmina37-dev <babazadehmina37@gmail.com>
2025-09-10 10:47:09 +0330

    add project files

---
+++ online-store/products.js
@@ -0,0 +1,202 @@
+
+export const products = [
+{ 
+    id: 1, 
+    name:"کلاه تابستانی", 
+    category: "کلاه",
+    price: 380000, 
+    shortDesc: "کلاه راحت ماسب هوای گرم و آفتابی", 
+    longDesc: "کلاه راحت مناسب هوای آفتابی و گرم", 
+    thumbnail: "pictures/kolah-tabestani.jpg", 
+    onHand: 10 
+},
+{ 
+    id: 2, 
+    name:"کلاه زمستانی", 
+    category: "کلاه",
+    price: 400000, 
+    shortDesc: "کلاه گرم برای روزهای سرد", 
+    longDesc: "کلاه گرم برای روزهای سرد", 
+    thumbnail: "pictures/kolah-zemestani.jpg", 
+    onHand: 15 
+},
+{ 
+    id: 3, 
+    name: "پیراهن گلدار", 
+    category: "پوشاک",
+    price: 350000, 
+    shortDesc: "پیراهن نرم و راحت", 
+    longDesc: "پیراهن نرم و راحت", 
+    thumbnail: "pictures/1.jpg", 
+    onHand: 12 
+},
+{ 
+    id: 4, 
+    name: "تی شرت دخترانه", 
+    category: "پوشاک",
+    price: 220000, 
+    shortDesc:"تیشرت زیبا برای دختران بالای 13 سال", 
+    longDesc: "تیشرت زیبا برای دختران بالای 13 سال", 
+    thumbnail: "pictures/tishirt-dokhtarane.jpg", 
+    onHand: 8 
+},
+{ 
+    id: 5, 
+    name: "کیف دستی صورتی", 
+    category: "کیف",
+    price: 400000, 
+    shortDesc: "کیف ترندی صورتی و خوشگل", 
+    longDesc: "کیف ترندی صورتی و خوشگل", 
+    thumbnail: "pictures/kif-sorati.jpg", 
+    onHand: 6 
+},
+{ 
+    id: 6, 
+    name: "کوله پشتی دخترانه", 
+    price: 520000, 
+    shortDesc: "کوله بزرگ و جادار", 
+    longDesc: "کوله بزرگ و جادار", 
+    thumbnail: "pictures/koleposhti.jpeg", 
+    onHand: 10 
+},
+{ 
+    id: 7, 
+    name: "کفش اسپرت سفید", 
+    category: "کفش",
+    price: 480000, 
+    shortDesc: "کفش راحت", 
+    longDesc: "کفش اسپرت با رویه نرم و سبک مناسب پیاده‌روی", 
+    thumbnail: "pictures/kafshsport.jpg", 
+    onHand: 14 
+},
+{ 
+    id: 8, 
+    name:"کفش مجلسی ", 
+    category: "کفش",
+    price:1700000, 
+    shortDesc: "کفش زیبا مجلسی", 
+    longDesc: "کفش زیبا مجلسی", 
+    thumbnail: "pictures/kafshmajlesi.jpg", 
+    onHand: 10 
+},
+{ 
+    id: 9, 
+    name:"کاپشن صورتی دخترانه", 
+    category: "پوشاک",
+    price: 1200000, 
+    shortDesc: "کاپشن گرم  برای هوای سرد", 
+    longDesc: "کاپشن گرم  برای هوای سرد", 
+    thumbnail: "pictures/kapshon.jpg", 
+    onHand: 5 
+},
+{ 
+    id: 10, 
+    name: "شلوارک دخترانه", 
+    category: "پوشاک",
+    price: 350000, 
+    shortDesc:"شورتک راحت", 
+    longDesc:"شورتک راحت", 
+    thumbnail: "pictures/shalvarak.jpg", 
+    onHand: 12 
+},
+{ 
+    id: 11, 
+    name: "هودی بنفش دخترانه", 
+    category: "پوشاک",
+    price: 700000, 
+    shortDesc: "هودی گرم", 
+    longDesc: "ژاکت بافتنی مناسب فصل پاییز و زمستان", 
+    thumbnail: "pictures/hodi.jpg", 
+    onHand: 8 
+},
+{ 
+    id: 12, 
+    name: "دامن قرمز",
+    category: "پوشاک", 
+    price: 550000, 
+    shortDesc: "دامن قشنگ قرمز", 
+    longDesc: "دامن قشنگ قرمز", 
+    thumbnail: "pictures/damanqermez.jpg", 
+    onHand: 15 
+},
+{ 
+    id: 13, 
+    name:"بافت ترندی آبی", 
+    category: "پوشاک",
+    price: 1200000, 
+    shortDesc: "ژاکت بافتنی مناسب فصل پاییز و زمستان", 
+    longDesc: "ژاکت بافتنی مناسب فصل پاییز و زمستان", 
+    thumbnail: "pictures/baftabi.jpg", 
+    onHand: 6 
+},
+{ 
+    id: 14, 
+    name: "کیف دستی ماچا", 
+    category: "کیف",
+    price: 800000, 
+    shortDesc: "کیف ترندی ماچا", 
+    longDesc: "کیف ترندی ماچا", 
+    thumbnail: "pictures/kifsabzmacha.jpg", 
+    onHand: 20 
+},
+{ 
+    id: 15, 
+    name: "کوله مشکی", 
+    category: "کیف",
+    price: 850000, 
+    shortDesc: "کوله مناسب دختران بالای 18 سال", 
+    longDesc: "کوله مناسب دختران بالای 18 سال", 
+    thumbnail: "pictures/kolemeshki.jpg", 
+    onHand: 7 
+},
+{ 
+    id: 16, 
+    name: "کوله خرگوشی صورتی", 
+    category: "کیف",
+    price: 2200000, 
+    shortDesc: "کوله ترندی خرگوشی", 
+    longDesc: "کوله ترندی خرگوشی", 
+    thumbnail: "pictures/kolesorati.jpg", 
+    onHand: 5 
+},
+{ 
+    id: 17, 
+    name: "کتونی مشکی سفید", 
+    category: "کفش",
+    price: 3500000, 
+    shortDesc:"کتونی نرم و راحت", 
+    longDesc: "کتونی نرم و راحت", 
+    thumbnail: "pictures/katonimeshkisefid.jpg", 
+    onHand: 6 
+},
+{ 
+    id: 18, 
+    name:"کتونی دخترانه کرم", 
+    category: "کفش",
+    price: 2800000, 
+    shortDesc: "کتونی دخترانه رنگ کرم", 
+    longDesc:  "کتونی دخترانه رنگ کرم", 
+    thumbnail: "pictures/katonikerem.jpg", 
+    onHand: 12 
+},
+{ 
+    id: 19, 
+    name: "کلاه نقاب دار", 
+    category: "کلاه",
+    price: 300000, 
+    shortDesc: "کلاه نقابدار برای خاص پسندا", 
+    longDesc: "کلاه نقابدار برای خاص پسندا", 
+    thumbnail: "pictures/kolahneqabdar.jpg", 
+    onHand: 8 
+},
+{ 
+    id: 20, 
+    name: "کلاه پهلوی دخترانه", 
+    category: "کلاه",
+    price: 650000, 
+    shortDesc: "کلاه پهلوی قرمز و جذاب", 
+    longDesc:"کلاه پهلوی قرمز و جذاب", 
+    thumbnail: "pictures/kolahpahlavi.jpg", 
+    onHand: 10 
+}
+];
